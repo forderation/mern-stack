e27d6ffd9800979f5aa7b878a7044d2aeff50d35
forderation <kharisma.muzaki@gmail.com>
2020-09-21 04:34:35 +0700

    init until modal

---
+++ frontend/src/places/components/PlaceList.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Card from "../../shared/components/UIElements/Card";
+import PlaceItem from "./PlaceItem";
+import { Link } from "react-router-dom";
+import "./PlaceList.css";
+
+const PlaceList = (props) => {
+  if (props.items.length === 0) {
+    return (
+      <div className="place-list center">
+        <Card>
+          <h2>No places found. Maybe create one ?</h2>
+          <Link to="/">
+            <button>Share Place</button>
+          </Link>
+        </Card>
+      </div>
+    );
+  }
+  return (
+    <ul className="place-list">
+      {props.items.map((place) => (
+        <PlaceItem
+          key={place.id}
+          id={place.id}
+          image={place.imageUrl}
+          description={place.description}
+          title={place.title}
+          address={place.address}
+          creator={place.creator}
+          coordinates={place.location}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default PlaceList;
