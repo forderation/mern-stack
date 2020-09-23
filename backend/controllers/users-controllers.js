1db809f739f10e602e8257dbeee5fab0ca2d0de4
forderation <kharisma.muzaki@gmail.com>
2020-09-23 19:39:49 +0700

    CRUD user & place

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -1,49 +1,82 @@
 const { v4: uuidv4 } = require("uuid");
 const HttpError = require("../models/http-error");
 const { validationResult } = require("express-validator");
+const User = require("../models/user");
 
-let USERS = [
-  {
-    id: "u2",
-    name: "Kharisma Muzaki",
-    email: "123@gmail.com",
-    password: "tester",
-  },
-  {
-    id: "u1",
-    name: "Max Schwarz",
-    email: "12356@gmail.com",
-    password: "tester",
-  },
-];
-
-const getUsers = (req, res, next) => {
-  res.json({ users: USERS });
+const getUsers = async (req, res, next) => {
+  let users;
+  try {
+    users = await User.find({}, "-password");
+  } catch (error) {
+    const err = new HttpError("Error when fetch user. please try again", 500);
+    return err;
+  }
+  if (users.length === 0) {
+    const err = new HttpError("Users collection is empty", 500);
+    return err;
+  }
+  res
+    .status(200)
+    .json({ users: users.map((u) => u.toObject({ getters: true })) });
 };
 
-const userSignUp = (req, res, next) => {
+const userSignUp = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    console.log(errors);
-    throw new HttpError("Invalid inputs passed, please check your data", 422);
+    const error = new HttpError(
+      "Invalid inputs passed, please check your data",
+      422
+    );
+    return next(error);
   }
-  const { name, password, email } = req.body;
-  const hasUser = USERS.find((u) => u.email === email);
-  if (hasUser) {
-    throw new HttpError("Email has already taken.", 422);
+  const { name, password, email, image } = req.body;
+
+  let exisingUser;
+
+  try {
+    exisingUser = await User.findOne({ email: email });
+  } catch (error) {
+    const err = new HttpError("Something wrong when fetch user", 500);
+    return next(err);
   }
-  const user = { id: uuidv4(), name, email, password };
-  USERS.push(user);
-  res.status(201).json({ user });
+
+  if (exisingUser) {
+    const error = new HttpError("Email has already taken", 422);
+    return next(error);
+  }
+
+  let newUser;
+  try {
+    newUser = await new User({ name, password, email, image, places: [] }).save();
+  } catch (error) {
+    const err = new HttpError("Something wrong when create user", 500);
+    return next(err);
+  }
+
+  res.status(201).json({ user: newUser.toObject({ getters: true }) });
 };
 
-const userSignIn = (req, res, next) => {
+const userSignIn = async (req, res, next) => {
   const { email, password } = req.body;
-  const identifiedUser = USERS.find((u) => u.email === email);
-  if (!identifiedUser || identifiedUser.password !== password) {
-    throw new HttpError("Email or password is wrong", 401);
+
+  let exisingUser;
+
+  try {
+    exisingUser = await User.findOne({ email: email, password: password });
+  } catch (error) {
+    const err = new HttpError("Something wrong when fetch user", 500);
+    return next(err);
   }
-  res.status(200).json({ message: "Logged in" });
+
+  if (!exisingUser) {
+    const error = new HttpError(
+      "Logging in failed !, Email or password is wrong!",
+      422
+    );
+    return next(error);
+  }
+
+  res.status(200).json({ user: exisingUser.toObject({ getters: true }) });
 };
 
 module.exports = { getUsers, userSignUp, userSignIn };
