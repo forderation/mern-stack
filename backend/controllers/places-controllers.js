8b2f765716044ab476edbe465f6d4d264772840e
forderation <kharisma.muzaki@gmail.com>
2020-09-23 15:25:37 +0700

    crud place

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -4,35 +4,6 @@ const { validationResult } = require("express-validator");
 const getCoordsForAddress = require("../utils/location");
 const Place = require("../models/place");
 
-let DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world!",
-    imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
-    address: "20 W 34th St, New York, NY 10001",
-    location: {
-      lat: 40.7484405,
-      lng: -73.9878584,
-    },
-    creator: "u1",
-  },
-  {
-    id: "p2",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world!",
-    imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
-    address: "20 W 34th St, New York, NY 10001",
-    location: {
-      lat: 40.7484405,
-      lng: -73.9878584,
-    },
-    creator: "u1",
-  },
-];
-
 const getPlaceById = async (req, res, next) => {
   placeId = req.params.pid;
   let place;
@@ -111,30 +82,64 @@ const createNewPlace = async (req, res, next) => {
   res.status(201).json({ place: createdPlace });
 };
 
-const updateExistingPlace = (req, res, next) => {
+const updateExistingPlace = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     console.log(errors);
-    throw new HttpError("Invalid inputs passed, please check your data", 422);
+    return next(
+      new HttpError("Invalid inputs passed, please check your data", 422)
+    );
   }
   const { title, description } = req.body;
   placeId = req.params.pid;
-  const place = { ...DUMMY_PLACES.find((p) => p.id === placeId) };
-  console.log(`PLACES PATCH ${place}`);
-  if (!place.id) {
-    // sync codes
-    throw new HttpError("Could not find place with given id.", 404);
+
+  let place;
+  try {
+    place = await Place.findById(placeId);
+  } catch (err) {
+    const error = new HttpError("Couldn't update place, id doesn't exist", 500);
+    return next(error);
   }
-  const placeIndex = DUMMY_PLACES.findIndex((p) => p.id === placeId);
+
   place.title = title;
   place.description = description;
-  DUMMY_PLACES[placeIndex] = place;
-  res.status(200).json({ place });
+  try {
+    place = await place.save();
+  } catch (err) {
+    const error = new HttpError(
+      "Couldn't update place somethings wrong with database",
+      500
+    );
+    return next(error);
+  }
+
+  res.status(200).json({ place: place.toObject({ getters: true }) });
 };
 
-const deleteExistingPlace = (req, res, next) => {
+const deleteExistingPlace = async (req, res, next) => {
   placeId = req.params.pid;
-  DUMMY_PLACES = DUMMY_PLACES.filter((p) => p.id !== placeId);
+
+  let place;
+  try {
+    place = await Place.findById(placeId);
+  } catch {
+    const error = new HttpError(
+      "Couldn't delete place, given id is not exist",
+      500
+    );
+    return next(error);
+  }
+
+  try {
+    await place.delete();
+  } catch {
+    const error = new HttpError(
+      "Couldn't delete place, something is wrong",
+      500
+    );
+    return next(error);
+  }
+
   res.status(200).json({ message: "Delete place successfully" });
 };
 
